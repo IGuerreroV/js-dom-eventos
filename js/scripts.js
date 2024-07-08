c8eb2ecba1258ca1c92c580fc8b9d177c9327e9d
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-07-07 23:19:58 -0500

    Reaccionar a Clicks en JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -56,3 +56,14 @@ console.log(5);
 window.onscroll = function() {
     console.log('scrolling...');
 }
+
+
+// Seleccionar elementos y asociarles un evento
+const btnEnviar = document.querySelector('.boton--primario')
+btnEnviar.addEventListener('click', function(evento) {
+    console.log(evento);
+    evento.preventDefault()
+
+    // Validar un formulario
+    console.log('enviando formulario');
+})
