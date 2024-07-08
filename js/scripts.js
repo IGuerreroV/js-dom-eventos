cf864e5faa559aeeb0cdbb7db2e8813c07eb09c5
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-07-07 22:24:52 -0500

    Seleccionar Elementos con querySelectorAll y getElementById

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,12 @@ heading.textContent = 'Nuevo Heading'
 console.log(heading);
 
 // querySelectroAll
+const enlaces = document.querySelectorAll('.navegacion a')
+enlaces[0].textContent = 'Nuevo texto para enlace'
+enlaces[0].classList.add('nueva-clase')
+enlaces[0].classList.remove('navegacion__enlace')
 
 
 // getElementById
+const heading2 = document.getElementById('heading')
+console.log(heading2);
