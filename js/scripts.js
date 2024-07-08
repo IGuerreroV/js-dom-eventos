f72a5e90156070b6e70ab79997194c57553c9046
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-07-07 22:41:59 -0500

    Crear HTML con createElement

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,27 @@ console.log(heading);
 const enlaces = document.querySelectorAll('.navegacion a')
 enlaces[0].textContent = 'Nuevo texto para enlace'
 enlaces[0].classList.add('nueva-clase')
-enlaces[0].classList.remove('navegacion__enlace')
+// enlaces[0].classList.remove('navegacion__enlace')
 
 
 // getElementById
 const heading2 = document.getElementById('heading')
 console.log(heading2);
+
+// Generar un nuevo enlace
+const nuevoEnlace = document.createElement('A')
+
+// Agreagr el href
+nuevoEnlace.href = 'nuevo-enlace.html'
+
+// Agregar el texto
+nuevoEnlace.textContent = 'Un Nuevo enlace'
+
+// Agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace')
+
+// Agregar al documento
+const navegacion = document.querySelector('.navegacion')
+navegacion.appendChild(nuevoEnlace)
+
+console.log(nuevoEnlace);
